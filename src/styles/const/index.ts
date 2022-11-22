e254e80719059d8ebfb70289a881a8a03313675f
佐藤肇伸 <toshinobu_sato@bita.jp>
2022-11-22 09:40:18 +0900

    add: design token defined

---
+++ src/styles/const/index.ts
@@ -0,0 +1,37 @@
+export const COLOR = {
+  ACCENT: "#0d87e0",
+  BLACK: "#222",
+  GRAY_10: "#eee",
+  GRAY_25: "#aaa",
+  GRAY_50: "#707070",
+  GRAY_75: "#444",
+  WHITE: "#fff",
+} as const
+
+export const WIDTH = {
+  CONTAINER: "1152px",
+  MAX: "1280px",
+  SP: "92%",
+} as const
+
+export const SPACE_PC = {
+  XS: "30px",
+  SM: "45px",
+  MD: "60px",
+  LG: "90px",
+  XL: "120px",
+  JUMP: "60px",
+} as const
+
+export const SPACE_SP = {
+  XS: "20px",
+  SM: "30px",
+  MD: "40px",
+  LG: "60px",
+  XL: "80px",
+  JUMP: "20px",
+} as const
+
+export const BREAKPOINT = {
+  SMP_MAXWIDTH: "600px",
+} as const
